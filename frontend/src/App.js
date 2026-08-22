d64db3f73e299b339b19967151dca6729c836020
Shivam Kumar <shivamkrpce@gmail.com>
2026-08-22 12:42:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     const employee = { name, designation };
 
     try {
-      const response = await fetch('http://13.206.77.26:9090/api/employees', {
+      const response = await fetch('http://3.108.195.215:9090/api/employees', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
